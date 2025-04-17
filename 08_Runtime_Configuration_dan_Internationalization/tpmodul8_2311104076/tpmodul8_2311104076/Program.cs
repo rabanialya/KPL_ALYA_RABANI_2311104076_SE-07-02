87073ff9dc1c7c4c40906ab289bc493369d196ac
alya <alyarabani33@gmail.com>
2025-04-17 23:10:54 +0700

    menyelesaikan program dengan runtime configuration

--- 08_Runtime_Configuration_dan_Internationalization/tpmodul8_2311104076/tpmodul8_2311104076/Program.cs
+++ 08_Runtime_Configuration_dan_Internationalization/tpmodul8_2311104076/tpmodul8_2311104076/Program.cs
@@ -1,2 +1,100 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.Json;
+using System.IO;
+
+public class CovidConfig
+{
+    public string satuan_suhu { get; set; }
+    public int batas_hari_deman { get; set; }
+    public string pesan_ditolak { get; set; }
+    public string pesan_diterima { get; set; }
+
+    private const string filePath = "covid_config.json";
+
+    // Nilai default
+    public CovidConfig()
+    {
+        satuan_suhu = "celcius";
+        batas_hari_deman = 14;
+        pesan_ditolak = "Anda tidak diperbolehkan masuk ke dalam gedung ini";
+        pesan_diterima = "Anda dipersilahkan untuk masuk ke dalam gedung ini";
+    }
+
+    public void LoadConfig()
+    {
+        if (File.Exists(filePath))
+        {
+            string jsonString = File.ReadAllText(filePath);
+            var config = JsonSerializer.Deserialize<CovidConfig>(jsonString);
+            satuan_suhu = config.satuan_suhu;
+            batas_hari_deman = config.batas_hari_deman;
+            pesan_ditolak = config.pesan_ditolak;
+            pesan_diterima = config.pesan_diterima;
+        }
+        else
+        {
+            SaveConfig(); // Simpan default config kalau belum ada file
+        }
+    }
+
+    public void SaveConfig()
+    {
+        string jsonString = JsonSerializer.Serialize(this, new JsonSerializerOptions { WriteIndented = true });
+        File.WriteAllText(filePath, jsonString);
+    }
+
+    public void UbahSatuan()
+    {
+        if (satuan_suhu.ToLower() == "celcius")
+        {
+            satuan_suhu = "fahrenheit";
+        }
+        else
+        {
+            satuan_suhu = "celcius";
+        }
+        SaveConfig(); // simpan perubahan satuan
+    }
+}
+
+
+class Program
+{
+    static void Main()
+    {
+        CovidConfig config = new CovidConfig();
+        config.LoadConfig();
+
+        // Ubah satuan dulu (sesuai poin G)
+        config.UbahSatuan();
+        Console.WriteLine("Satuan suhu sekarang: " + config.satuan_suhu);
+
+        Console.Write($"Berapa suhu badan anda saat ini? Dalam nilai {config.satuan_suhu}: ");
+        double suhu = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Berapa hari yang lalu (perkiraan) anda terakhir memiliki gejala demam? ");
+        int hariDemam = Convert.ToInt32(Console.ReadLine());
+
+        bool suhuNormal = false;
+
+        if (config.satuan_suhu.ToLower() == "celcius")
+        {
+            suhuNormal = suhu >= 36.5 && suhu <= 37.5;
+        }
+        else if (config.satuan_suhu.ToLower() == "fahrenheit")
+        {
+            suhuNormal = suhu >= 97.7 && suhu <= 99.5;
+        }
+
+        bool hariValid = hariDemam < config.batas_hari_deman;
+
+        if (suhuNormal && hariValid)
+        {
+            Console.WriteLine(config.pesan_diterima);
+        }
+        else
+        {
+            Console.WriteLine(config.pesan_ditolak);
+        }
+    }
+}
+
--- 08_Runtime_Configuration_dan_Internationalization/tpmodul8_2311104076/tpmodul8_2311104076/obj/Debug/net8.0/tpmodul8_2311104076.AssemblyInfo.cs
+++ 08_Runtime_Configuration_dan_Internationalization/tpmodul8_2311104076/tpmodul8_2311104076/obj/Debug/net8.0/tpmodul8_2311104076.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tpmodul8_2311104076")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00587b88409814959226cab361b8fc81e603e201")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5872eb48d5b691f37ad604e42a542868a2dea091")]
 [assembly: System.Reflection.AssemblyProductAttribute("tpmodul8_2311104076")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tpmodul8_2311104076")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
