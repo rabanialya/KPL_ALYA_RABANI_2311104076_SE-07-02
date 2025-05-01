602d0081e49e1509482e79f1bdb0b3e33a61c443
alya <alyarabani33@gmail.com>
2025-05-01 23:36:46 +0700

    menambahkan tpmod10

---
+++ 10_Library_Construction/tpmodul10_2311104076/AljabarTester/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using AljabarLibraries;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Aljabar aljabar = new Aljabar();
+
+        // Contoh 1: Akar persamaan kuadrat x^2 - 3x - 10 = 0
+        double[] akar = aljabar.AkarPersamaanKuadrat(new double[] { 1, -3, -10 });
+        Console.WriteLine("Akar-akarnya: {0}, {1}", akar[0], akar[1]);
+
+        // Contoh 2: Hasil kuadrat dari 2x - 3
+        double[] kuadrat = aljabar.HasilKuadrat(new double[] { 2, -3 });
+        Console.WriteLine("Hasil kuadrat: {0}x^2 + {1}x + {2}", kuadrat[0], kuadrat[1], kuadrat[2]);
+    }
+}
