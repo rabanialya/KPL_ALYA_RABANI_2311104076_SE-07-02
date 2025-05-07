43b9bcbf00708b7948a79f4875f77b805ec3d6c8
alya <alyarabani33@gmail.com>
2025-05-08 01:00:11 +0700

    nambah tjmod 10

---
+++ 10_Library_Construction/tjmodul10_2311104076/ShowCase/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using MatematikaLibraries;
+
+namespace ShowCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Matematika math = new Matematika();
+
+            Console.WriteLine("FPB 60 & 45 = " + math.FPB(60, 45));
+            Console.WriteLine("KPK 12 & 8 = " + math.KPK(12, 8));
+
+            int[] persamaan1 = { 1, 4, -12, 9 };
+            Console.WriteLine("Turunan dari x^3 + 4x^2 - 12x + 9 adalah: ");
+            Console.WriteLine(math.Turunan(persamaan1));
+
+            int[] persamaan2 = { 4, 6, -12, 9 };
+            Console.WriteLine("Integral dari 4x^3 + 6x^2 - 12x + 9 adalah: ");
+            Console.WriteLine(math.Integral(persamaan2));
+
+            Console.ReadLine();
+        }
+    }
+}
