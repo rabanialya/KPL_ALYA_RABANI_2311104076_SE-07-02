9901620ab7bdce1322e4a2bd6565c8e5b4c1a8d6
alya <alyarabani33@gmail.com>
2025-05-09 10:05:59 +0700

    nambah guided

---
+++ 12_Performance_Analysis_Unit_Testing_dan_Debugging/modul12/UnitTest/MSTestSettings.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void TestGrade()
+        {
+            String? result1 = modul12.Program.DetermineGrade(90);
+            Assert.AreEqual("A", result1);
+
+            String? result2 = modul12.Program.DetermineGrade(80);
+            Assert.AreEqual("B", result2);
+
+            String? result3 = modul12.Program.DetermineGrade(70);
+            Assert.AreEqual("C", result3);
+
+            String? result4 = modul12.Program.DetermineGrade(60);
+            Assert.AreEqual("D", result4);
+
+            String? result5 = modul12.Program.DetermineGrade(50);
+            Assert.AreEqual("E", result5);
+
+            String? result6 = modul12.Program.DetermineGrade(101);
+            Assert.IsNull(result6);
+
+            String? result7 = modul12.Program.DetermineGrade(-1);
+            Assert.IsNull(result7);
+
+        }
+    }
+}
